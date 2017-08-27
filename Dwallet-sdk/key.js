81338db17974545a58841ef6d8f16f2b4601b738
dsfund <dsfund@163.com>
2017-08-27 22:43:50 +0800

    original

---
+++ Dwallet-sdk/key.js
@@ -0,0 +1,12 @@
+var ethUtil = require('ethereumjs-util');
+ethUtil.crypto = require('crypto');
+
+exports.generateKeyPair = function() {
+    var privKey = ethUtil.crypto.randomBytes(32);
+    var privKeyHex = ethUtil.bufferToHex(privKey);
+    var publKeyHex = ethUtil.bufferToHex(ethUtil.privateToAddress(privKey));
+    return {
+        'privateKey' : privKeyHex.substr(2),
+        'publicKey' : publKeyHex
+    }
+};

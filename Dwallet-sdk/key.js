fe5877dcb171a0262a04738ab10aa0e83cb5c199
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-08-30 08:37:03 +0800

    update keystore json

--- Dwallet-sdk/key.js
+++ Dwallet-sdk/key.js
@@ -4,6 +4,8 @@ var ethKeys = require("ethereumjs-keys");
 
 ethUtil.crypto = require('crypto');
 
+
+
 exports.generateKeyPair = function() {
     var privKey = ethUtil.crypto.randomBytes(32);
     var privKeyHex = ethUtil.bufferToHex(privKey);
@@ -13,3 +15,21 @@ exports.generateKeyPair = function() {
         'publicKey' : publKeyHex
     }
 };
+
+exports.generateKeyJson = function(){
+	let password = "wanglu"
+
+	// Key derivation function (default: PBKDF2) 
+	var kdf = "scrypt"; // slow
+	//var kdf = "pbkdf2"; // fast
+	 
+	// Generate private key and the salt and initialization vector to encrypt it 
+	var dk = ethKeys.create();
+	// Export key data to keystore "secret-storage" format: 
+	// https://github.com/ethereum/wiki/wiki/Web3-Secret-Storage-Definition 
+	var json = ethKeys.dump(password, dk.privateKey, dk.salt, dk.iv, kdf);
+	console.log(json);
+	return json;
+}
+
+exports.generateKeyJson();
